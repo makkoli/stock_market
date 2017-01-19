8ffcfc75a4ed14b68d9f6b37485a596040259cb2
makkoli <makkoli@vt.edu>
2017-01-19 04:42:56 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    MongoClient = require('mongodb').MongoClient,
+    bodyParser = require('body-parser'),
+    helmet = require('helmet'),
+    mongoose = require('mongoose'),
+    site = require('./routes/site')
+    env = require('./env/env'),
+    app = express();
+
+var dbConnStr = 'mongodb://localhost:27017/stock';
+// Connect mongoose to db
+mongoose.connect(dbConnStr, function(err) {
+    if (err) throw err;
+    console.log('Connected to mongodb');
+});
+
+// Length of cache for static files
+var oneDay = 86400000;
+
+// static files
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+// parse body for POST sent by user
+app.use(bodyParser.urlencoded({ extended: true }));
+// views
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
+// helmet for security
+app.use(helmet());
+
+
+// Home page
+app.get('/', site.index);
+
+// https://api.intrinio.com/historical_data?identifier=AAPL&item=close_price&start_date=2016-01-01
+
+var server = app.listen(8000, function() {
+    var port = server.address().port;
+    console.log('Express server listening on port %s.', port);
+});
---
+++ routes/site.js
@@ -0,0 +1,4 @@
+// Render index page
+exports.index = function(req, res) {
+    res.render('index');
+}
